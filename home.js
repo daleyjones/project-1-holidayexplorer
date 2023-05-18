99c00821bf7a996f8941c33e741c50ea01571b39
faduma abdi <faduma_abdi@hotmail.co.uk>
2023-05-18 19:45:04 +0100

    fadumacommitratings

---
+++ home.js
@@ -0,0 +1,23 @@
+// Select all elements with the "i" tag and store them in a NodeList called "stars"
+const stars = document.querySelectorAll(".stars i");
+// Loop through the "stars" NodeList
+stars.forEach((star, index1) => {
+    let rating = 0;
+    // Add an event listener that runs a function when the "click" event is triggered
+    star.addEventListener("click", () => {
+        // Loop through the "stars" NodeList Again
+        stars.forEach((star, index2) => {
+            // Add the "active" class to the clicked star and any stars with a lower index
+            // and remove the "active" class from any stars with a higher index
+            if (index1 >= index2) {
+                star.classList.add("active")
+                rating++;
+            }
+            else {
+                star.classList.remove("active");
+            }
+        });
+
+        document.getElementById("rating-value").textContent = rating
+    });
+});

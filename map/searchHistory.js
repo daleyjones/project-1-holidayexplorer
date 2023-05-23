c3e7714f525ef93020464e7e90d8b720b30777cb
daleyjones <djones_15@hotmail.com>
2023-05-23 19:49:06 +0100

    new commit

---
+++ map/searchHistory.js
@@ -0,0 +1,43 @@
+// Retrieve the search history from local storage
+function getSearchHistory() {
+    var searchHistory = localStorage.getItem('searchHistory');
+    if (searchHistory) {
+      return JSON.parse(searchHistory);
+    }
+    return [];
+  }
+  
+  // Save search history to local storage
+  function saveSearchHistory(searchText) {
+    var searchHistory = getSearchHistory();
+    searchHistory.push(searchText);
+    localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+  }
+  
+  // Display the search history
+  function displaySearchHistory() {
+    var searchHistory = getSearchHistory();
+    var searchList = document.getElementById('searchList');
+    searchList.innerHTML = ''; // Clear the list first
+    searchHistory.forEach(function (searchText) {
+      var searchItem = document.createElement('li');
+      searchItem.textContent = searchText;
+      searchList.appendChild(searchItem);
+    });
+  }
+  
+  // Attach event listener to the search button
+  var searchButton = document.getElementById('searchButton');
+  searchButton.addEventListener('click', function () {
+    var searchText = document.getElementById('searchText').value;
+    console.log(searchText);
+    lookUpLocation(searchText);
+    saveSearchHistory(searchText);
+    displaySearchHistory(); // Update the search history display
+  });
+  
+  // Load search history from local storage when the page loads
+  window.addEventListener('DOMContentLoaded', function () {
+    displaySearchHistory();
+  });
+  
